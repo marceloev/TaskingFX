d970738eda41c28b14806ea01220e1fb4a88f163
marceloev <marcelo.sinergia@outlook.com>
2018-07-20 00:01:55 -0300

    go
    go

--- src/main/java/com/taskingfx/TaskingFX.java
+++ src/main/java/com/taskingfx/TaskingFX.java
@@ -1,13 +1,15 @@
 package com.taskingfx;
 
-import com.taskingfx.util.log.GravaLog;
+import com.taskingfx.DAO.UserDAO;
+import com.taskingfx.entitys.User;
+import com.taskingfx.util.log.TaskingLog;
 
 public class TaskingFX {
 
     public static void main(String[] args) {
-        /*UserDAO dao = new UserDAO();
-        User user = dao.findAll().get(0);
-        System.out.println(user.toString());*/
-        GravaLog.gravaInfo(TaskingFX.class, "teste1");
+        UserDAO userDAO = new UserDAO();
+        User user = userDAO.findByPK(1);
+        System.out.println(user.toString());
+        TaskingLog.gravaInfo(TaskingFX.class, user.toString());
     }
 }
---
+++ src/main/java/com/taskingfx/util/log/TaskingLog.java
@@ -0,0 +1,40 @@
+package com.taskingfx.util.log;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import java.net.URL;
+
+public class TaskingLog {
+
+    private static final Logger logger = Logger.getLogger(TaskingLog.class);
+    private static final URL urlProperties = TaskingLog.class.getResource("/statics/properties/log4j.properties");
+    private static Boolean configurado = false;
+
+    public static void gravaInfo(Class invoker, String info) {
+        if (isConfigurado())
+            logger.info("[" + invoker.getName() + "] : " + info);
+    }
+
+    public static void gravaAlerta(Class invoker, String info) {
+        if (isConfigurado())
+            logger.warn("[" + invoker.getName() + "] : " + info);
+    }
+
+    public static void gravaErro(Class invoker, String info) {
+        if (isConfigurado())
+            logger.error("[" + invoker.getName() + "] : " + info);
+    }
+
+    public static void gravaErro(Class invoker, String info, Throwable trhows) {
+        if (isConfigurado())
+            logger.error("[" + invoker.getName() + "] : " + info, trhows);
+    }
+
+    private static Boolean isConfigurado() {
+        if (!configurado)
+            PropertyConfigurator.configure(urlProperties);
+        configurado = true;
+        return true;
+    }
+}
